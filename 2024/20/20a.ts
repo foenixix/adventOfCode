bcab1bee95ec39e73933bc990b17ce1e1bc7a5fd
Foenixix <valentin@cvbhome.be>
2024-12-23 23:44:29 +0100

    finally understood the assignment of day 20

--- 2024/20/20a.ts
+++ 2024/20/20a.ts
@@ -1,4 +1,5 @@
 import INPUTS from "./20.json";
+import { writeFileSync } from "fs";
 
 const map = INPUTS.value;
 const width = map[0].length;
@@ -22,186 +23,109 @@ map.forEach((row, y) => {
 function toPos(x: number, y: number): string {
   return x + "," + y;
 }
-//find the fastest way to get there
-function getFirstInfo() {
-  let currentSteps = 0;
-  let currentVisits: [[number, number], string[]][] = [
-    [start, [toPos(start[0], start[1])]],
+
+const originalRoute: string[] = [];
+let startX = start[0];
+let startY = start[1];
+while (startX !== end[0] || startY !== end[1]) {
+  originalRoute.push(toPos(startX, startY));
+  const nextCandidates: [number, number][] = [
+    [startX, startY + 1],
+    [startX, startY - 1],
+    [startX + 1, startY],
+    [startX - 1, startY],
   ];
-  const nextVisits = new Map<string, string[]>();
-  const visitedSet = new Set<string>();
-  const reachMap = new Map<string, number>();
-
-  function step(): boolean {
-    for (const [[x, y], route] of currentVisits) {
-      const pos = toPos(x, y);
-      visitedSet.add(pos);
-      if (x === end[0] && y === end[1]) {
-        for (let i = 0; i < route.length; i++) {
-          reachMap.set(route[route.length - 1 - i], i);
-        }
-        return true;
-      }
-
-      const nextCandidates: [number, number][] = [
-        [x, y + 1],
-        [x, y - 1],
-        [x + 1, y],
-        [x - 1, y],
-      ];
-      for (const candidate of nextCandidates) {
-        if (
-          candidate[0] < 1 ||
-          candidate[1] < 1 ||
-          candidate[0] >= width - 1 ||
-          candidate[1] >= height - 1
-        ) {
-          continue;
-        }
-        const pos = toPos(candidate[0], candidate[1]);
-        if (map[candidate[1]][candidate[0]] !== "#" && !visitedSet.has(pos)) {
-          nextVisits.set(pos, [...route, pos]);
-        }
-      }
+  for (const candidate of nextCandidates) {
+    if (
+      candidate[0] < 1 ||
+      candidate[1] < 1 ||
+      candidate[0] >= width - 1 ||
+      candidate[1] >= height - 1 ||
+      map[candidate[1]][candidate[0]] === "#" ||
+      originalRoute.at(-2) === toPos(candidate[0], candidate[1])
+    ) {
+      continue;
     }
-    return false;
+    startX = candidate[0];
+    startY = candidate[1];
   }
-  while (currentVisits.length > 0) {
-    if (step()) {
-      return { steps: currentSteps, reachMap, visitedSet };
-    } else {
-      currentVisits = [...nextVisits.entries()].map(([visit, route]) => {
-        const [stringX, stringY] = visit.split(",");
-        return [[parseInt(stringX), parseInt(stringY)], route];
-      });
-      nextVisits.clear();
-    }
-    currentSteps++;
-  }
-  throw new Error("Illegal state, should've found steps in while loop");
 }
+originalRoute.push(toPos(end[0], end[1]));
 
-const { steps: maxSteps, reachMap: originalReachMap } = getFirstInfo();
-console.log("max steps", maxSteps);
+console.log("route length", originalRoute.length);
 
-let currentSteps = 0;
-let currentVisits: [[number, number], boolean, string[]][] = [
-  [start, false, []],
-];
-let nextVisits: [[number, number], boolean, string[]][] = [];
-let routes = 0;
+const cheatsPos = new Set<string>();
+const cheatsFound: Record<number, number> = {};
 
-function step() {
-  for (const [[x, y], cheated, route] of currentVisits) {
-    const nextRoute = [...route, toPos(x, y)];
-    if (x === end[0] && y === end[1]) {
-      routes++;
+function lookForCheat(x: number, y: number, currentSteps: number) {
+  const nextCandidates: [number, number][] = [
+    [x, y + 1],
+    [x, y - 1],
+    [x + 1, y],
+    [x - 1, y],
+  ];
+  for (const candidate of nextCandidates) {
+    if (
+      candidate[0] < 1 ||
+      candidate[1] < 1 ||
+      candidate[0] >= width - 1 ||
+      candidate[1] >= height - 1 ||
+      map[candidate[1]][candidate[0]] === "#"
+    ) {
+      continue;
     }
-
-    const nextCandidates: [number, number][] = [
-      [x, y + 1],
-      [x, y - 1],
-      [x + 1, y],
-      [x - 1, y],
-    ];
-    for (const candidate of nextCandidates) {
-      if (
-        candidate[0] < 1 ||
-        candidate[1] < 1 ||
-        candidate[0] >= width - 1 ||
-        candidate[1] >= height - 1 ||
-        route.includes(toPos(candidate[0], candidate[1]))
-      ) {
-        continue;
+    const candidatePos = toPos(candidate[0], candidate[1]);
+    const originalSteps = originalRoute.indexOf(candidatePos);
+    if (originalSteps < 0) {
+      throw new Error(
+        "Illegal State, original should have passed here: " + candidatePos,
+      );
+    }
+    if (originalSteps > currentSteps + 1) {
+      cheatsPos.add(toPos(x, y));
+      const savedSteps = originalSteps - currentSteps - 1;
+      if (!cheatsFound[savedSteps]) {
+        cheatsFound[savedSteps] = 0;
       }
-
-      if (cheated) {
-        if (map[candidate[1]][candidate[0]] !== "#") {
-          nextVisits.push([candidate, true, nextRoute]);
-        }
-      } else {
-        const cheating = map[candidate[1]][candidate[0]] === "#";
-        nextVisits.push([candidate, cheating, nextRoute]);
-      }
+      cheatsFound[savedSteps]++;
     }
   }
 }
-while (currentVisits.length > 0 && currentSteps < maxSteps) {
-  console.log(i, currentVisits.length);
-  step();
-  currentVisits = nextVisits;
-  nextVisits = [];
-  currentSteps++;
-}
-console.log(routes);
 
-// function toVisit(x: number, y: number, cheated: boolean, steps: number) {
-//   return `${x},${y},${cheated},${steps}`;
-// }
-// const reachMap = new Map<string, string[]>();
-// //TODO: populate the reachmap with the originalReachMap
+originalRoute.forEach((pos, i) => {
+  const [stringX, stringY] = pos.split(",");
+  const x = parseInt(stringX);
+  const y = parseInt(stringY);
 
-// function getRoutes(
-//   x: number,
-//   y: number,
-//   cheated: boolean,
-//   steps: number,
-//   route: string[],
-// ): string[] {
-//   if (steps >= maxSteps) {
-//     return [];
-//   } else if (Math.abs(end[0] - x) + Math.abs(end[1] - y) >= maxSteps - steps) {
-//     return [];
-//   }
-//   const nextRoute = [...route, toPos(x, y)];
-//   if (x === end[0] && y === end[1]) {
-//     console.log("route", nextRoute);
-//     return nextRoute;
-//   }
-//   const visit = toVisit(x, y, cheated, steps);
-//   // if (reachMap.has(visit)) {
-//   //   return reachMap.get(visit)!;
-//   // }
+  const nextCandidates: [number, number][] = [
+    [x, y + 1],
+    [x, y - 1],
+    [x + 1, y],
+    [x - 1, y],
+  ];
+  for (const candidate of nextCandidates) {
+    if (
+      candidate[0] < 1 ||
+      candidate[1] < 1 ||
+      candidate[0] >= width - 1 ||
+      candidate[1] >= height - 1
+    ) {
+      continue;
+    }
+    if (map[candidate[1]][candidate[0]] === "#") {
+      lookForCheat(candidate[0], candidate[1], i + 1);
+    }
+  }
+});
 
-//   let routes = [];
-//   const nextCandidates: [number, number][] = [
-//     [x, y + 1],
-//     [x, y - 1],
-//     [x + 1, y],
-//     [x - 1, y],
-//   ];
-//   for (const candidate of nextCandidates) {
-//     if (
-//       candidate[0] < 1 ||
-//       candidate[1] < 1 ||
-//       candidate[0] >= width - 1 ||
-//       candidate[1] >= height - 1 ||
-//       route.includes(toPos(candidate[0], candidate[1]))
-//     ) {
-//       continue;
-//     }
-//     if (cheated) {
-//       if (map[candidate[1]][candidate[0]] !== "#") {
-//         routes.push(
-//           ...getRoutes(candidate[0], candidate[1], true, steps + 1, nextRoute),
-//         );
-//       }
-//     } else {
-//       const cheating = map[candidate[1]][candidate[0]] === "#";
-//       routes.push(
-//         ...getRoutes(
-//           candidate[0],
-//           candidate[1],
-//           cheating,
-//           steps + 1,
-//           nextRoute,
-//         ),
-//       );
-//     }
-//   }
-//   reachMap.set(visit, routes);
-//   return count;
-// }
-
-// console.log(countRoutes(start[0], start[1], false, 0, []));
-// debugger;
+const totalCheats = Object.entries(cheatsFound)
+  .filter(([key]) => parseInt(key) >= 100)
+  .map(([key, value]) => value)
+  .reduce((acc, value) => acc + value, 0);
+console.log(totalCheats);
+console.log("check", cheatsPos.size);
+console.log(cheatsFound);
+console.log(
+  Object.entries(cheatsFound).filter(([key]) => parseInt(key) >= 100),
+);
+debugger;
